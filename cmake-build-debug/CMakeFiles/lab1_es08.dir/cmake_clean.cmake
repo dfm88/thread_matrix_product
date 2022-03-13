6555001db44f7e47d2288d61487fce85b8dac70d
dfm88 <diegof.university@gmail.com>
2022-03-13 19:01:50 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/lab1_es08.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_es08.dir/main.c.o"
+  "lab1_es08"
+  "lab1_es08.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab1_es08.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
